1a0699f337a35d7a5eecba940c413ed8fd7d1030
Shirley Zhu <shirleyzhu@gmail.com>
2021-07-28 17:46:04 +0000

    view notes

---
+++ starter/firebaseNotes/public/js/viewNotes.js
@@ -0,0 +1,43 @@
+window.onload = (event) => {
+  // Use this to retain user state between html pages.
+  firebase.auth().onAuthStateChanged(function(user) {
+      
+    if (user) {
+      googleUser = user;
+      getNotes(googleUser.uid);
+    } else {
+      window.location = 'index.html'; // If not logged in, navigate back to login page.
+    }
+  });
+};
+
+const getNotes = (userId) => {
+    // 1) Get access to all the current user's notes 
+    const dbRef = firebase.database().ref(`users/${userId}`);
+    dbRef.on('value', (snapshot) => {
+        renderData(snapshot.val());
+    })
+    // 2) Display them on the page
+}
+
+const renderData = (data) => {
+    const destination = document.querySelector('#app');
+    destination.innerHTML = "";
+    for (let noteKey in data) {
+        const note = data[noteKey];
+        destination.innerHTML += createCard(note);
+    }
+}
+
+const createCard  = (note) => {
+    return `<div class="column is-one-quarter"> 
+                <div class="card"> 
+                    <header class="card-header">
+                        <p class="card-header-title">${note.title}</p>
+                    </header> 
+                    <div class="card-content">
+                        ${note.text}
+                    </div> 
+                </div>
+            </div>` ;
+}
--- starter/firebaseNotes/public/js/writeNote.js
+++ starter/firebaseNotes/public/js/writeNote.js
@@ -15,6 +15,18 @@ window.onload = (event) => {
 const handleNoteSubmit = () => {
     console.log("note submission function called");
     // 1. Capture the form data
+    const noteTitle = document.querySelector("#noteTitle");
+    const noteText = document.querySelector("#noteText");
+
     // 2. Format the data and write it to our database
+    const note = {
+        title: noteTitle.value,
+        text: noteText.value
+    }
+    firebase.database().ref(`users/${googleUser.uid}`).push(note)
     // 3. Clear the form so that we can write a new note
+    .then(() => {
+        noteTitle.value = null;
+        noteText.value = null;
+    });
 }
